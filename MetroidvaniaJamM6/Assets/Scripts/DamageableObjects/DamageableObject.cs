1513b7c5505f02e739f29b964a3a34c5f39a4365
Josef21296 <jcasanovasv@hotmail.com>
2019-11-24 13:56:32 +0100

    Small fixes and changes

--- MetroidvaniaJamM6/Assets/Scripts/DamageableObjects/DamageableObject.cs
+++ MetroidvaniaJamM6/Assets/Scripts/DamageableObjects/DamageableObject.cs
@@ -3,10 +3,14 @@ using UnityEngine.Events;
 
 public class DamageableObject : MonoBehaviour
 {
+	public float CurrentHealth => m_currentHealth;
+	public float MaxHealth => m_maxHealth;
+	public float CurrentHealthPercentage => m_currentHealth / m_maxHealth;
+	
 	[SerializeField] protected float m_maxHealth = 10f;
 	[SerializeField] protected float m_initialHealth = 10f;
 
-	[Tooltip("Arg0: Health modification, Arg1: New health percentage, Arg2: Is damage")]
+	[Tooltip("Arg0: New health, Arg1: New health percentage, Arg2: Change cause")]
 	[SerializeField] protected HealthChangedEvent m_onHealthChanged = null;
 	[SerializeField] protected DeathEvent m_onDeath = null;
 
@@ -26,11 +30,21 @@ public class DamageableObject : MonoBehaviour
 	{
 		float lastHealth = m_currentHealth;
 		m_currentHealth += amount;
-		m_onHealthChanged?.Invoke(m_currentHealth, m_currentHealth / m_maxHealth, m_currentHealth < lastHealth);
+		m_onHealthChanged?.Invoke(m_currentHealth, CurrentHealthPercentage, m_currentHealth < lastHealth ? HealthChangeCause.Damage : HealthChangeCause.Heal);
 		
 		if (m_currentHealth <= 0f)
 		{
 			m_onDeath?.Invoke();
+		}
+	}
+
+	public void Set(float maxHealth, float currentHealth, bool notify = true)
+	{
+		m_maxHealth = maxHealth;
+		m_currentHealth = currentHealth;
+		if (notify)
+		{
+			m_onHealthChanged?.Invoke(m_currentHealth, CurrentHealthPercentage, HealthChangeCause.None);
 		}
 	}
 
@@ -41,9 +55,16 @@ public class DamageableObject : MonoBehaviour
 
 	protected void Start()
 	{
-		m_onHealthChanged?.Invoke(m_currentHealth, m_currentHealth / m_maxHealth, false);
+		m_onHealthChanged?.Invoke(m_currentHealth, m_currentHealth / m_maxHealth, HealthChangeCause.None);
 	}
 
-	[System.Serializable] protected class HealthChangedEvent : UnityEvent<float, float, bool> { }
+	public enum HealthChangeCause
+	{
+		None,
+		Damage,
+		Heal
+	};
+	
+	[System.Serializable] protected class HealthChangedEvent : UnityEvent<float, float, HealthChangeCause> { }
 	[System.Serializable] protected class DeathEvent : UnityEvent { }
 }
