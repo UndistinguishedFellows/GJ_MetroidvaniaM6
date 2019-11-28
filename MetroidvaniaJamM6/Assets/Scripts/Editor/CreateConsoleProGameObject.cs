1bc845ba011877d0bcd33bfee7f5e69d55383182
CapitanLiteral <anaraguen@gmail.com>
2019-11-28 01:38:27 +0100

    Ojo lo malament que se'm dona fer que les coses es moguin...

---
+++ MetroidvaniaJamM6/Assets/Scripts/Editor/CreateConsoleProGameObject.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+	public static class CreateConsoleProGameObject
+	{
+		[MenuItem("GameObject/ConsolePro/Instantiate console prefab", false, 0)]
+		public static void InstantiateConsole()
+		{
+			var assets = AssetDatabase.FindAssets("DebugConsole");
+			string path;
+			foreach (var asset in assets)
+			{
+				path = AssetDatabase.GUIDToAssetPath(asset);
+				string extension = Path.GetExtension(path);
+				if (extension.Equals(".prefab"))
+				{
+					GameObject prefab = AssetDatabase.LoadAssetAtPath(path, typeof(GameObject)) as GameObject;
+					PrefabUtility.InstantiatePrefab(prefab);
+				}
+			}
+		}
+	}
+}
---
+++ MetroidvaniaJamM6/Assets/Scripts/Gameplay.Runtime/Enemies/ExplosiveEnemy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class ExplosiveEnemy : MonoBehaviour
+{
+    private DamageableObject damageReceiver; //Make this a list?
+    private Tween damage;
+    private Tween move;
+    [SerializeField] private Transform target;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Triger enter");
+        if (other.gameObject.CompareTag($"DamegeableObject"))
+        {
+            damageReceiver = other.gameObject.GetComponent<DamageableObject>();
+            float progress = 0;
+            damage = DOTween.To(() => progress, x => progress = x, 1, 2);
+            damage.OnComplete(Damage);
+            damage.Play();
+            move.Pause();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Trigger exit");
+        if (other.gameObject.GetComponent<DamageableObject>() == damageReceiver)
+        {
+            damageReceiver = null;
+            damage.Kill();
+            move = transform.DOMove(target.position, 2);
+        }
+    }
+
+    private void Damage()
+    {
+        damageReceiver.Damage(50);
+        Debug.Log("damageReceiver.CurrentHealth = " + damageReceiver.CurrentHealth);
+        Destroy(gameObject);
+    }
+    private void Start()
+    {
+        move = transform.DOMove(target.position, 2);
+    }
+}
