7d4171f226cf90b9d3d1c8b0cda73092e1b7adeb
Josef21296 <jcasanovasv@hotmail.com>
2019-12-01 20:31:25 +0100

    Small test

---
+++ MetroidvaniaJamM6/Assets/Scripts/Gameplay.Runtime/InputSystem/Test/InputSystemTester.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class InputSystemTester : MonoBehaviour
+{
+    public void OnGameInputEventA()
+    {
+        Debug.Log("Game input event <b>A</b>");
+    }
+    
+    public void OnGameInputEventB()
+    {
+        Debug.Log("Game input event <b>B</b>");
+    }
+}
