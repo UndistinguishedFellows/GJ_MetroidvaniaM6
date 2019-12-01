05d250878e215045184b139f2a9fa8e9be619ed5
Josef21296 <jcasanovasv@hotmail.com>
2019-12-01 20:55:50 +0100

    Minor changes to force merge

--- MetroidvaniaJamM6/Assets/Scripts/Gameplay.Runtime/InputSystem/GameInputEvent.cs
+++ MetroidvaniaJamM6/Assets/Scripts/Gameplay.Runtime/InputSystem/GameInputEvent.cs
@@ -24,3 +24,4 @@ public class GameInputEvent : SerializedScriptableObject
     [SerializeField] private HashSet<InputEventBase> m_inputEvents = new HashSet<InputEventBase>();
     [SerializeField] private HashSet<AxisInputEvent> m_axisInputEvents = new HashSet<AxisInputEvent>();
 }
+
--- MetroidvaniaJamM6/Assets/Scripts/Gameplay.Runtime/InputSystem/InputProcessor.cs
+++ MetroidvaniaJamM6/Assets/Scripts/Gameplay.Runtime/InputSystem/InputProcessor.cs
@@ -111,3 +111,4 @@ public class InputProcessor : MonoBehaviour
     private Dictionary<GameInputEvent, List<IInputReceiver>> m_gameInputsCallbacks = new Dictionary<GameInputEvent, List<IInputReceiver>>();
     private Dictionary<AxisInputEvent, List<IInputReceiver>> m_axisCallbacks = new Dictionary<AxisInputEvent, List<IInputReceiver>>();
 }
+
